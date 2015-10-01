41a2e00eeb1fc1b1f1f4b208945a9de08facdf08
Moises <47767573t@iespoblenou.org>
2015-10-01 20:23:37 +0200

    clase completa 151001

---
+++ EclipseWorkSpace/M06/UF01/src/Practica2/Enjaular.java
@@ -0,0 +1,25 @@
+package Practica2;
+
+import java.io.File;
+
+public class Enjaular {
+	
+	public void guardar (Animal a, String path){	
+		
+	}
+	
+	public void guardar (Animal a, File fichero){	
+		
+	}
+	
+	public Animal soltar (String path){
+		Animal an = new Animal();
+		return an;
+	}
+	
+	public Animal soltar (File fichero){
+		Animal an = new Animal();
+		return an;
+	}
+
+}
